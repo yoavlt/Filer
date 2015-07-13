d0f08cdeee5772b9c96f56b0e911c37eae904c8e
Takuma Yoshida <yoa.jmpr.w@gmail.com>
2015-07-13 09:43:15 +0900

    :heavy_plus_sign: add test

--- FilerTests/FilerTests.swift
+++ FilerTests/FilerTests.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import XCTest
+import Filer
 
 class FilerTests: XCTestCase {
     
@@ -24,6 +25,31 @@ class FilerTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         XCTAssert(true, "Pass")
+    }
+    
+    func testCreateDeleteDirectory() {
+        XCTAssert(Filer.mkdir(.DocumentDirectory, dirName: "hoge"), "create directory successfuly")
+        XCTAssert(Filer.exists(.DocumentDirectory, path: "hoge"), "exists diretory")
+        XCTAssert(Filer.rmdir(.DocumentDirectory, dirName: "hoge"), "delete directory successfuly")
+        XCTAssertFalse(Filer.exists(.DocumentDirectory, path: "hoge"), "didn't exists diretory")
+    }
+    
+    func testReadWriteFile() {
+        let file = Filer(fileName: "test.txt")
+        FileWriter(file: file).write("test!!")
+        XCTAssertEqual(FileReader(file: file).read(), "test!!", "write body correctly")
+        XCTAssert(file.delete(), "delete file")
+        XCTAssertFalse(file.isExists, "test delete file")
+    }
+    
+    func testCopyFile() {
+        let file = Filer(fileName: "test.txt")
+        FileWriter(file: file).write("test!!")
+        XCTAssert(Filer.cp(.DocumentDirectory, srcPath: "test.txt", toPath: "test2.txt"), "copy file successfuly")
+        XCTAssert(Filer.exists(.DocumentDirectory, path: "test.txt"), "test dupliated")
+        XCTAssert(Filer.exists(.DocumentDirectory, path: "test2.txt"), "test dupliated")
+        XCTAssert(Filer.rm(.DocumentDirectory, path: "test.txt"), "delete file")
+        XCTAssert(Filer.rm(.DocumentDirectory, path: "test2.txt"), "delete file")
     }
     
     func testPerformanceExample() {
